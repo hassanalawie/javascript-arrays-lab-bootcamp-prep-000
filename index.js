a8efdcdd33a7b1c44400977dc622fc74374ca7ca
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-18 14:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function Arrays(){
-  function kittens(){
-var kittens = ['Milo', 'Otis', 'Garfield'] 
-  return kittens
+  function kittens()
+  {
+    var kittens = ['Milo', 'Otis', 'Garfield'] 
+    return kittens
   }
 //define your array here
-Arrays();
+
 // Add your functions and code here
 function destructivelyAppendKitten(kittens){
 kittens.push("Ralph")

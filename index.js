a969ff7900166ac46d969334fbc6b4829dae0509
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-18 14:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function Arrays(){
+
 kittens = ['Milo', 'Otis', 'Garfield'] 
-}
+
 //define your array here
-Arrays();
+
 // Add your functions and code here
-function destructivelyAppendKitten(kittens){
+
 kittens.push("Ralph")
-}
+
 
 
